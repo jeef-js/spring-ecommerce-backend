d736fb18196c267323accb6a2b9ee06ddd067cbc
Jefferson Araujo <jeffersonthiago.ga@gmail.com>
2024-01-26 13:03:40 -0300

    feat: criando estrutura inicial do projeto

---
+++ src/main/java/br/com/ecommerce/backend/EComerceBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComerceBackendApplication.class, args);
+	}
+
+}
